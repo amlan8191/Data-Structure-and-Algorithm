f2b4ee233a1377c4eedae794ac6a77e75e2cca4d
amlan8191 <amlankalita05@gmail.com>
2024-12-10 11:29:53 +0530

    Pattern using loop

--- Basics/test.cpp
+++ Basics/test.cpp
@@ -1,16 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main(){
-    int T;
-    cin>>T;
+    int size;
+    cout<<"ENter the size of the array: "<<endl;
 
-    while (T--)
-    {
-        /* code */
-        int a,b;
-        cin>>a>>b;
-        cout<<a%b<<endl;
-    }
+    cin>>size;
+
+    int arr[size];
+
+    cout<<"Fill up the array: "<<endl;
     
+    for(int i=0;i<size;i++){
+        
+        cin>>arr[i];
+    }
+    for(int i=0;i<size;i++){
+        cout<<"Element at "<<i<< " values are "<<arr[i]<<endl;
+    }
 }
